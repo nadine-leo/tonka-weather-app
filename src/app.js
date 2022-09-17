d9404cc740b0b9313f5a3dc86946f0c06183152c
nadine-leo <112672535+nadine-leo@users.noreply.github.com>
2022-09-17 23:07:41 +0100

    added conversion celsius-fahrenheit

--- src/app.js
+++ src/app.js
@@ -29,19 +29,7 @@ function convertTime(time) {
 //Get Current Location and info      //////////////
 
 function showCurrentTemp(response) {
-  console.log(response);
-  let currTemp = Math.round(response.data.main.temp);
-  let minT = Math.round(response.data.main.temp_min);
-  let maxT = Math.round(response.data.main.temp_max);
-  let feelsT = Math.round(response.data.main.feels_like);
-  let windSpeed = Math.round(response.data.wind.speed);
-  let humidity = Math.round(response.data.main.humidity);
-  let visibility = Math.round(response.data.visibility * 10) / 10000;
-  let timeValueUnix = response.data.dt;
-  let timezone = response.data.timezone;
-  let timeValue = (timeValueUnix + timezone) * 1000;
-
-  let tempToday = document.querySelector(".weather-temperature strong");
+  let tempToday = document.querySelector("#current-temperature");
   let weatherDescription = document.querySelector(".short-description");
   let cityName = document.querySelector("#city-name");
   let countryName = document.querySelector("#country-name");
@@ -55,23 +43,33 @@ function showCurrentTemp(response) {
   let currVisibility = document.querySelector("#visibility");
   let weatherIcon = document.querySelector("#icon");
 
-  tempToday.innerHTML = `${currTemp}`;
+  let timeValueUnix = response.data.dt;
+  let timezone = response.data.timezone;
+  let timeValue = (timeValueUnix + timezone) * 1000;
+
+  celsius = response.data.main.temp;
+  celsiusMax = response.data.main.temp_max;
+  celsiusMin = response.data.main.temp_min;
+  celsiusFeel = response.data.main.feels_like;
+
+  tempToday.innerHTML = Math.round(celsius);
   weatherDescription.innerHTML = response.data.weather[0].main;
   longWeatherDescription.innerHTML = response.data.weather[0].description;
   cityName.innerHTML = response.data.name;
   countryName.innerHTML = response.data.sys.country;
-  minTemp.innerHTML = minT;
-  maxTemp.innerHTML = maxT;
-  otherMaxTemp.innerHTML = maxT;
-  feelsLikeT.innerHTML = feelsT;
-  currHumidity.innerHTML = humidity;
-  currVisibility.innerHTML = visibility;
-  currWindSpeed.innerHTML = windSpeed;
+  minTemp.innerHTML = Math.round(celsiusMin);
+  maxTemp.innerHTML = Math.round(celsiusMax);
+  otherMaxTemp.innerHTML = Math.round(celsiusMax);
+  feelsLikeT.innerHTML = Math.round(celsiusFeel);
+  currHumidity.innerHTML = Math.round(response.data.main.humidity);
+  currVisibility.innerHTML = Math.round(response.data.visibility * 10) / 10000;
+  currWindSpeed.innerHTML = Math.round(response.data.wind.speed);
   weatherIcon.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIcon.setAttribute("alt", response.data.weather[0].description);
+
   convertTime(timeValue);
 }
 
@@ -89,9 +87,13 @@ function callNavigator() {
   //cityInput.value = "";
 }
 
-callNavigator();
-
 //Weather API Location     ///////////////////////
+function changeUnitstoC() {
+  let units = document.querySelectorAll(".unit");
+  units.forEach((placement) => {
+    placement.innerHTML = " °C";
+  });
+}
 
 function callWeatherApi(city) {
   let apiKey = "257e74eb60b897c439a9569203b9000a";
@@ -104,7 +106,65 @@ function changeCurrentCity(event) {
   let cityInput = document.querySelector("#search-input");
   let inputValue = cityInput.value;
   callWeatherApi(inputValue);
+  changeUnitstoC();
 }
+
+function showCelsiusTemp(event) {
+  event.preventDefault();
+
+  let currentTemp = document.querySelector("#current-temperature");
+  let minTemp = document.querySelector("#curr-min-temp");
+  let maxTemp = document.querySelector("#curr-max-temp");
+  let otherMaxTemp = document.querySelector("#max-temp");
+  let feelTemp = document.querySelector(".feels-like-temp");
+
+  currentTemp.innerHTML = Math.round(celsius);
+  minTemp.innerHTML = Math.round(celsiusMin);
+  maxTemp.innerHTML = Math.round(celsiusMax);
+  otherMaxTemp.innerHTML = Math.round(celsiusMax);
+  feelTemp.innerHTML = Math.round(celsiusFeel);
+
+  changeUnitstoC();
+}
+
+function showFahrenheitTemp(event) {
+  event.preventDefault();
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  let fahrenheitMin = (celsiusMin * 9) / 5 + 32;
+  let fahrenheitMax = (celsiusMax * 9) / 5 + 32;
+  let fahrenheitFeel = (celsiusFeel * 9) / 5 + 32;
+
+  let currentTemp = document.querySelector("#current-temperature");
+  let minTemp = document.querySelector("#curr-min-temp");
+  let maxTemp = document.querySelector("#curr-max-temp");
+  let otherMaxTemp = document.querySelector("#max-temp");
+  let feelTemp = document.querySelector(".feels-like-temp");
+
+  let units = document.querySelectorAll(".unit");
+
+  currentTemp.innerHTML = Math.round(fahrenheit);
+  minTemp.innerHTML = Math.round(fahrenheitMin);
+  maxTemp.innerHTML = Math.round(fahrenheitMax);
+  otherMaxTemp.innerHTML = Math.round(fahrenheitMax);
+  feelTemp.innerHTML = Math.round(fahrenheitFeel);
+  units.forEach((placement) => {
+    placement.innerHTML = " °F";
+  });
+}
+
+let celsius = null;
+let celsiusMax = null;
+let celsiusMin = null;
+let celsiusFeel = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", changeCurrentCity);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsiusTemp);
+///////function calls///////
+
+callNavigator();
